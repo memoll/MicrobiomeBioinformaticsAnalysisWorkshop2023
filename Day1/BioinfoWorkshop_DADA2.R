9a2ab808eef14daad8b7f9fce141525f4ab1d6f1
Mona Parizadeh <memoll@users.noreply.github.com>
2023-03-20 22:33:46 -0600

    Update BioinfoWorkshop_DADA2.R

--- Day1/BioinfoWorkshop_DADA2.R
+++ Day1/BioinfoWorkshop_DADA2.R
@@ -3,7 +3,7 @@
 # Based on DADA2 tutorial by Benjamin Callahan                #
 # Data: Mice - Antibiotic experience                          #
 # By: ArrietaLab - University of Calgary                      #
-# Author: Mackenzie Gutierrez                                 # 
+# Author: Emily Mercer                                        # 
 # Dates: 3-5 April 2023                                       #
 # Location: Oswaldo-Cruz Institute - Rio de Janeiro (Brazil)  #
 ###############################################################
